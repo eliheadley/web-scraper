e9b508040b00fea434207661802a1d1721cbaf0f
eliheadley <eliheadley@gmail.com>
2024-03-09 21:17:56 -0500

    Basic html file and parser

--- web_scraper.py
+++ web_scraper.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from bs4 import BeautifulSoup
+import requests
+
+# Open the html file
+with open("page.html", "r") as f:
+    html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    for tag in soup.find_all("ul"):
+        print(tag.text)
